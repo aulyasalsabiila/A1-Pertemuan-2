3665b43639f739ccc68577aa4ccae649801cdd7e
aulyasalsabiila <128215223+aulyasalsabiila@users.noreply.github.com>
2024-03-04 20:48:47 +0700

    Add files via upload

--- 24060122140163/res/shader/super_basic.fs
+++ 24060122140163/res/shader/super_basic.fs
@@ -1,10 +1,13 @@
-#version 330 core
+# version 330 core
+
+// in vec4 position;
 
 // Output data
 layout(location = 0) out vec4 color;
 
 void main(){
 
-	// Output color, warna yang akan digambar ke layar
+	// Output color = color of the texture at the specified UV
+	// color = vec4(gl_FragCoord., 0, 1); //vec4(1., 0., 0., 1.);
 	color = vec4(1., 0., 0., 1.);
 }
